b540b08f01df5d3ed5370fcc721315a0ef1adce7
tititopher68-dev2 <ctophe68.dev2@gmail.com>
2022-05-19 13:16:42 +0200

    Refactor  common Script Cura V4 and V5 JPEG preview

--- Slicer post-processing scripts for Gcode Preview/Cura/Cura_JPEG_Preview.py
+++ Slicer post-processing scripts for Gcode Preview/Cura/Cura_JPEG_Preview.py
@@ -1,10 +1,10 @@
-# Contains code from: https://github.com/mriscoc/Marlin_Ender3v2/blob/42585074807fa799bdee7ced10c9d83508df6ebf/slicer%20scripts/cura/CreateJPEGThumbnail.py
-
+# By Lchristophe68 - contains some codes from Cura thumbnails plugin
+# For Cura 4.x.x and Cura 5.x
 import base64
 
 from UM.Logger import Logger
 from cura.Snapshot import Snapshot
-from PyQt5.QtCore import QByteArray, QIODevice, QBuffer
+from cura.CuraVersion import CuraVersion
 
 from ..Script import Script
 
@@ -27,6 +27,20 @@ class Cura_JPEG_Preview(Script):
             thumbnail_buffer.open(QBuffer.ReadWrite)
             thumbnail_image = snapshot
             thumbnail_image.save(thumbnail_buffer, "JPG", quality)
+            base64_bytes = base64.b64encode(thumbnail_buffer.data())
+            base64_message = base64_bytes.decode('ascii')
+            thumbnail_buffer.close()
+            return base64_message
+        except Exception:
+            Logger.logException("w", "Failed to encode snapshot image")
+    
+    def _encodeSnapshot_v5(self, snapshot):
+        Logger.log("d", "Encoding thumbnail image...")
+        try:
+            thumbnail_buffer = QBuffer()
+            thumbnail_buffer.open(QBuffer.OpenModeFlag.ReadWrite)
+            thumbnail_image = snapshot
+            thumbnail_image.save(thumbnail_buffer, "JPG")
             base64_bytes = base64.b64encode(thumbnail_buffer.data())
             base64_message = base64_bytes.decode('ascii')
             thumbnail_buffer.close()
@@ -78,6 +92,20 @@ class Cura_JPEG_Preview(Script):
         }"""
 
     def execute(self, data):
+    
+        Major=0
+        Minor=0
+        try:
+          Major = int(CuraVersion.split(".")[0])
+          Minor = int(CuraVersion.split(".")[1])
+        except:
+          pass
+        
+        if Major < 5 :
+          from PyQt5.QtCore import QByteArray, QIODevice, QBuffer
+        else :
+          from PyQt6.QtCore import QByteArray, QIODevice, QBuffer
+        
         thumbnail_width = 50
         thumbnail_height = 50
         preview_width = 180
@@ -85,7 +113,10 @@ class Cura_JPEG_Preview(Script):
 
         preview = self._createSnapshot(preview_width, preview_height)
         if preview and self.getSettingValueByKey("create_preview"):
-            encoded_preview = self._encodeSnapshot(preview, 60)
+            if Major < 5 :
+              encoded_preview = self._encodeSnapshot(preview, 60)
+            else :
+              encoded_preview = self._encodeSnapshot_v5(preview)              
             preview_gcode = self._convertSnapshotToGcode(
                 encoded_preview, preview_width, preview_height)
 
@@ -104,7 +135,10 @@ class Cura_JPEG_Preview(Script):
 
         thumbnail = self._createSnapshot(thumbnail_width, thumbnail_height)
         if thumbnail and self.getSettingValueByKey("create_thumbnail"):
-            encoded_thumbnail = self._encodeSnapshot(thumbnail, 80)
+            if Major < 5 :
+              encoded_thumbnail = self._encodeSnapshot(thumbnail, 80)
+            else :
+              encoded_thumbnail = self._encodeSnapshot_v5(thumbnail)
             thumbnail_gcode = self._convertSnapshotToGcode(
                 encoded_thumbnail, thumbnail_width, thumbnail_height)
 
